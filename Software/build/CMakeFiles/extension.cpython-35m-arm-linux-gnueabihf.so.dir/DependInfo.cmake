3d982e81bc78147acfaddf84f12456f979bfd65b
eddymj96 <eddymj96@googlemail.com>
2019-03-31 04:18:54 +0100

    Added CMake file which should allow for compilation on any user's RPI. Dependencies are requires however and the target architecture of the RPI has been hardcoded for now.

---
+++ Software/build/CMakeFiles/extension.cpython-35m-arm-linux-gnueabihf.so.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/automated-bar/Software/src/MultiServoControl.cpp" "/home/pi/automated-bar/Software/build/CMakeFiles/extension.cpython-35m-arm-linux-gnueabihf.so.dir/MultiServoControl.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.5m"
+  "/usr/local/include/python3.5"
+  "/home/pi/.local/include/python3.5m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
