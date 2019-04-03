c3b6b91c79f90266c7885dfa0b655d2d0396f7d8
eddy <eddymj96@googlemail.com>
2019-04-04 00:26:18 +0100

    Travisci build attempt

---
+++ Software/dependencies.sh
@@ -0,0 +1,13 @@
+cd src/
+git clone git://git.drogon.net/wiringPi
+cd wiringPi
+./build
+cd ../
+git clone https://github.com/Reinbert/pca9685.git
+cd pca9685/src
+make -j
+cd ../
+pip3 install pybind11
+
+
+
