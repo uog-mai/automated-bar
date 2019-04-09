f4e2a3c9097579d558bd3e96fc2530cd5880e1f3
eddymj96 <eddymj96@googlemail.com>
2019-04-09 22:45:45 +0100

    Empty Python Library source.

---
+++ Firmware/src/PyDrinkServer.cpp
@@ -0,0 +1,17 @@
+#include <string>
+
+#include "DrinksConfig.h"
+
+using namespace mai;
+
+DrinksConfig drinks_config;
+
+bool dispense_drink(const std::string &drink, const int volume_ml) {
+    // TODO
+    return true;
+}
+
+bool replace_drink(const int position, const std::string &new_drink) {
+    // TODO
+    return true;
+}
