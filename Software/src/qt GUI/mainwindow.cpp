15acc3dd9ce72b1482d73410b39a64b8a8bea064
jrmars <joe.marsden10@gmail.com>
2019-04-13 16:19:03 +0100

    formatting to gui

--- Software/src/qt GUI/mainwindow.cpp
+++ Software/src/qt GUI/mainwindow.cpp
@@ -63,50 +63,59 @@ MainWindow::MainWindow(QWidget *parent) :
     QTextStream p8(&pos8);
 
     //ui->textBrowser->setText("<font size = 6>Vodka</font>");
+    ui->textBrowser->setFontWeight(60);
+    ui->textBrowser->setFontPointSize(16);
     ui->textBrowser->setText(p1.readAll());
-    ui->textBrowser->setFontWeight(6);
     ui->textBrowser->setAlignment(Qt::AlignCenter);
     ui->textBrowser->setFrameStyle(QFrame::NoFrame);
 
+
     //ui->textBrowser_2->setText("<font size = 6><b>Gin</b></font>");
+    ui->textBrowser_2->setFontWeight(60);
+    ui->textBrowser_2->setFontPointSize(16);
     ui->textBrowser_2->setText(p2.readAll());
-    ui->textBrowser_2->setFontWeight(6);
     ui->textBrowser_2->setAlignment(Qt::AlignCenter);
     ui->textBrowser_2->setFrameStyle(QFrame::NoFrame);
 
     //ui->textBrowser_3->setText("<font size = 6><i>Rum</i></font>");
+    ui->textBrowser_3->setFontWeight(60);
+    ui->textBrowser_3->setFontPointSize(16);
     ui->textBrowser_3->setText(p3.readAll());
-    ui->textBrowser_3->setFontWeight(6);
     ui->textBrowser_3->setAlignment(Qt::AlignCenter);
     ui->textBrowser_3->setFrameStyle(QFrame::NoFrame);
 
     //ui->textBrowser_4->setText("<font size = 6><i>Buckfast</i></font>");
+    ui->textBrowser_4->setFontWeight(60);
+    ui->textBrowser_4->setFontPointSize(16);
     ui->textBrowser_4->setText(p4.readAll());
-    ui->textBrowser_4->setFontWeight(6);
     ui->textBrowser_4->setAlignment(Qt::AlignCenter);
     ui->textBrowser_4->setFrameStyle(QFrame::NoFrame);
 
     //ui->textBrowser_5->setText("<font size = 5><b><i>Coca Cola</b></i></font>");
+    ui->textBrowser_5->setFontWeight(60);
+    ui->textBrowser_5->setFontPointSize(16);
     ui->textBrowser_5->setText(p5.readAll());
-    ui->textBrowser_5->setFontWeight(6);
     ui->textBrowser_5->setAlignment(Qt::AlignCenter);
     ui->textBrowser_5->setFrameStyle(QFrame::NoFrame);
 
     //ui->textBrowser_6->setText("<font size = 5><b><i>Lemonade</b></i></font>");
+    ui->textBrowser_6->setFontWeight(60);
+    ui->textBrowser_6->setFontPointSize(16);
     ui->textBrowser_6->setText(p6.readAll());
-    ui->textBrowser_6->setFontWeight(6);
     ui->textBrowser_6->setAlignment(Qt::AlignCenter);
     ui->textBrowser_6->setFrameStyle(QFrame::NoFrame);
 
    // ui->textBrowser_7->setText("<font size = 5><b><i>Irn Bru</b></i></font>");
+    ui->textBrowser_7->setFontWeight(60);
+    ui->textBrowser_7->setFontPointSize(16);
     ui->textBrowser_7->setText(p7.readAll());
-    ui->textBrowser_7->setFontWeight(6);
     ui->textBrowser_7->setAlignment(Qt::AlignCenter);
     ui->textBrowser_7->setFrameStyle(QFrame::NoFrame);
 
     //ui->textBrowser_8->setText("<font size = 5><b><i>Tonic</b></i></font>");
+    ui->textBrowser_8->setFontWeight(60);
+    ui->textBrowser_8->setFontPointSize(16);
     ui->textBrowser_8->setText(p8.readAll());
-    ui->textBrowser_8->setFontWeight(6);
     ui->textBrowser_8->setAlignment(Qt::AlignCenter);
     ui->textBrowser_8->setFrameStyle(QFrame::NoFrame);
 }
